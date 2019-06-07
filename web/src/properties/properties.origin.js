60056115eb6f4bc2519485fed7d4fa131fcbed5d
Aidar Shaifutdinov <aidarshaifutdinov@gmail.com>
2019-06-07 20:26:06 +0300

    Update properties origin file

--- web/src/properties/properties.origin.js
+++ web/src/properties/properties.origin.js
@@ -1,1 +1,4 @@
-export const IPFS_API_SERVER = '';
+export const IPFS_API_SERVER = ''; 
+export const CONTRACT_ADDRESS = ''; // CareerTracker contract address
+export const CONTRACTS_URL = ''; // /contracts/{role}/{address} api server endpoint
+export const IPFS_API_SERVER = ''; // /contracts/consider api server endpoint
